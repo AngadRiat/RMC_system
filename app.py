2de5ce0076650b1996eeb4152152907b4d55c6e1
Angad Riat <riatangad21@gmail.com>
2025-03-13 23:05:13 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -29,6 +29,8 @@ DB_PARAMS = {
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # MISC Functions used
 
